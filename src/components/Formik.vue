8c4dc4401b60a9cc99bed19bce1d82557c835dde
MaximeMalecot <92maximemalecot@gmail.com>
2022-12-16 12:25:31 +0100

    Init commit

---
+++ src/components/Formik.vue
@@ -0,0 +1,21 @@
+<script setup>
+    const formData = reactive({});
+    const submit = (data) => {
+        console.log(data)
+    }
+    const input = (key, value) => {
+        formData[key] = value
+        console.log(key, value)
+    }
+
+    provide('form:submit', submit)
+    provide('form:input', input)
+</script>
+
+<template>
+    <slot :submit="submit" :input="input"></slot>
+</template>
+
+<style>
+    
+</style>
