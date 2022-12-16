b847270ad2cd60f454e34d0d5abc6cab04847f4a
MaximeMalecot <92maximemalecot@gmail.com>
2022-12-16 12:57:55 +0100

    Testers implemented in App.vue and beginning of Field logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
- import { Formik } from './formik';
+ import { Formik, Field } from './formik';
 
 </script>
 
 <template>
   <main>
-    <Formik/>
+    <Formik>
+      <form>
+        <Field name="name" as="input"/>
+        <Field name="email" as="input"/>
+        <Field name="options" as="select">
+          <option value="1">test1</option>
+          <option value="2">test2</option>
+          <option value="3">test3</option>
+        </Field>
+        <Field name="captcha" as="Captcha" />
+
+      </form>
+    </Formik>
   </main>
 </template>
 
--- src/formik/components/Field.vue
+++ src/formik/components/Field.vue
@@ -1,8 +1,22 @@
 <script setup>
-</script>
+import { defineProps} from "vue";
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    as: {
+        type: String || Object,
+        default: "input",
+        required: true,
+    },
+})
+</script>   
 
 <template>
-
+    <component v-if="(typeof as === 'string')" :is="as" :name="name"/>
+    <component v-else :is="as" :name="name" />
 </template>
 
 <style>
