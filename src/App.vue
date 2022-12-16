0062bf5f5bea53e97d680a52c30dece6a338184e
= <julian.saleix@gmail.com>
2022-12-16 12:59:52 +0100

    Formik component in progress

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script setup>
  import { Formik } from './formik';
 
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const validate = values => {
+  const errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  }
+
+  const onSubmit= (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  }
 </script>
 
 <template>
   <main>
-    <Formik/>
+    <Formik 
+      :initialValues="initialValues" 
+      :validate="validate" 
+      :onSubmit="onSubmit">
+      <form>
+        <Field name="name" as="input"/>
+        <Field name="email" as="input"/>
+        <Field name="options" as="select">
+          <option value="1">test1</option>
+          <option value="2">test2</option>
+          <option value="3">test3</option>
+        </Field>
+        <Field name="captcha" as="Captcha" />
+
+      </form>
+    </Formik>
   </main>
 </template>
 
--- src/formik/components/Formik.vue
+++ src/formik/components/Formik.vue
@@ -1,33 +1,45 @@
 <script setup>
-import { defineProps, reactive, provide } from 'vue';
+    import { defineProps, reactive, provide, ref, watch } from 'vue';
 
-const props = defineProps({
-  initialValues: {
-    type: Object,
-    required: true,
-  },
-  validate: {
-    type: Function,
-    required: true,
-  },
-  onSubmit: {
-    type: Function,
-    required: true,
-  },
+    const props = defineProps({
+    initialValues: {
+        type: Object,
+        required: true,
+    },
+    validate: {
+        type: Function,
+        required: true,
+    },
+    onSubmit: {
+        type: Function,
+        required: true,
+    },
 
-});
+    });
 
-const formData = reactive({});
+    const values = ref(props.initialValues);
+    const isSubmitting = ref(false);
+    const setSubmitting = (value) => {
+        isSubmitting.value = value;
+    }
+    watch( () => {
+        if(isSubmitting.value) {
+            const errors = props.validate(values.value);
+            if(Object.keys(errors).length === 0) {
+                props.onSubmit(values.value, { setSubmitting });
+            }
+        }
+    }, isSubmitting.value);
+
+    const formData = reactive({});
     const submit = (data) => {
-        console.log(data)
-    }
-    const input = (key, value) => {
-        formData[key] = value
-        console.log(key, value)
+        console.log(data);
+        props.onSubmit(data, { setSubmitting });
     }
 
-    provide('form:submit', submit)
-    provide('form:input', input)
+
+    provide('form:submit', submit);
+    provide('form:isSubmitting', isSubmitting);
 </script>
 
 <template>
